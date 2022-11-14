3019c0bedd363b2a0efd15ae56088d4b7e594c7e
FengChen <fengtum.chen@tum.de>
2022-11-14 17:11:50 +0100

    Initialization of odd rviz plugin

---
+++ odd_visualizer/odd_rviz_plugin/src/odd_details.cpp
@@ -0,0 +1,6 @@
+#include "odd_details.hpp"
+
+namespace odd_rviz_plugin {
+class ODDdetailsDisplay
+: public rviz_common::Panel
+}
---
+++ odd_visualizer/odd_rviz_plugin/src/odd_details.hpp
@@ -0,0 +1,12 @@
+#ifndef ODD_DETAILS_HPP_
+#define ODD_DETAILS_HPP_
+
+#include <QLabel>
+#include <QPushButton>
+#include <QSpinBox>
+#include <rclcpp/rclcpp.hpp>
+#include <rviz_common/panel.hpp>
+
+
+
+#endif // ODD_DETAILS_HPP_
