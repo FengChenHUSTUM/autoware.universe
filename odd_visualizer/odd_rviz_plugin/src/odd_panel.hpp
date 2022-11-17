97ce761fb0171342437d81a5f6ad6b44ff217ba1
FengChen <fengtum.chen@tum.de>
2022-11-17 16:07:13 +0100

    structure desgin of ODD panel

--- odd_visualizer/odd_rviz_plugin/src/odd_panel.hpp
+++ odd_visualizer/odd_rviz_plugin/src/odd_panel.hpp
@@ -1,13 +1,17 @@
 #ifndef ODD_PANEL_HPP_
 #define ODD_PANEL_HPP_
 
+
+#include <regex>
 #include <QLabel>
 #include <QPushButton>
-#include <QSpinBox>
+#include <QTableWidget> 
 #include <rclcpp/rclcpp.hpp>
 #include <rviz_common/panel.hpp>
 
 #include <autoware_auto_system_msgs/msg/autoware_state.hpp>
+#include <scenery_msgs/msg/odd_elements.hpp>
+#include <scenery_msgs/srv/teleoperation.hpp>
 
 
 /* TODO: populate the panel function
@@ -31,52 +35,59 @@ public:
   void onInitialize() override;
 
 public Q_SLOTS:  // NOLINT for Qt
-  void onClickAutowareEngage();
-  void onClickVelocityLimit();
-  void onClickGateMode();
-  void onClickPathChangeApproval();
-  void onClickEmergencyButton();
+  // void onClickAutowareEngage();
+  // void onClickVelocityLimit();
+  // void onClickGateMode();
+  // void onClickPathChangeApproval();
+  // void onClickEmergencyButton();
+
+  void onClickODDTeleoperation();
 
 protected:
-  void onGateMode(const tier4_control_msgs::msg::GateMode::ConstSharedPtr msg);
-  void onSelectorMode(
-    const tier4_control_msgs::msg::ExternalCommandSelectorMode::ConstSharedPtr msg);
-  void onAutowareState(const autoware_auto_system_msgs::msg::AutowareState::ConstSharedPtr msg);
-  void onShift(const autoware_auto_vehicle_msgs::msg::GearReport::ConstSharedPtr msg);
-  void onEmergencyStatus(const tier4_external_api_msgs::msg::Emergency::ConstSharedPtr msg);
-  void onEngageStatus(const tier4_external_api_msgs::msg::EngageStatus::ConstSharedPtr msg);
+  void onODDSub(const scenery_msgs::msg::ODDElements::ConstSharedPtr msg);
+
+
+  // void onGateMode(const tier4_control_msgs::msg::GateMode::ConstSharedPtr msg);
+  // void onSelectorMode(
+  //   const tier4_control_msgs::msg::ExternalCommandSelectorMode::ConstSharedPtr msg);
+  // void onAutowareState(const autoware_auto_system_msgs::msg::AutowareState::ConstSharedPtr msg);
+  // void onShift(const autoware_auto_vehicle_msgs::msg::GearReport::ConstSharedPtr msg);
+  // void onEmergencyStatus(const tier4_external_api_msgs::msg::Emergency::ConstSharedPtr msg);
+  // void onEngageStatus(const tier4_external_api_msgs::msg::EngageStatus::ConstSharedPtr msg);
 
   rclcpp::Node::SharedPtr raw_node_;
-  rclcpp::Subscription<tier4_control_msgs::msg::GateMode>::SharedPtr sub_gate_mode_;
-  rclcpp::Subscription<tier4_control_msgs::msg::ExternalCommandSelectorMode>::SharedPtr
-    sub_selector_mode_;
-  rclcpp::Subscription<autoware_auto_system_msgs::msg::AutowareState>::SharedPtr
-    sub_autoware_state_;
-  rclcpp::Subscription<autoware_auto_vehicle_msgs::msg::GearReport>::SharedPtr sub_gear_;
-  rclcpp::Subscription<tier4_external_api_msgs::msg::EngageStatus>::SharedPtr sub_engage_;
+  rclcpp::Subscription<scenery_msgs::msg::ODDElements>::SharedPtr sub_odd_elements_;
+  rclcpp::Client<scenery_msgs::srv::Teleoperation>::SharedPtr client_teleoperation_;
 
-  rclcpp::Client<tier4_external_api_msgs::srv::Engage>::SharedPtr client_engage_;
-  rclcpp::Client<tier4_external_api_msgs::srv::SetEmergency>::SharedPtr client_emergency_stop_;
-  rclcpp::Subscription<tier4_external_api_msgs::msg::Emergency>::SharedPtr sub_emergency_;
 
-  rclcpp::Publisher<tier4_planning_msgs::msg::VelocityLimit>::SharedPtr pub_velocity_limit_;
-  rclcpp::Publisher<tier4_control_msgs::msg::GateMode>::SharedPtr pub_gate_mode_;
-  rclcpp::Publisher<tier4_planning_msgs::msg::Approval>::SharedPtr pub_path_change_approval_;
 
-  QLabel * gate_mode_label_ptr_;
-  QLabel * selector_mode_label_ptr_;
-  QLabel * autoware_state_label_ptr_;
-  QLabel * gear_label_ptr_;
-  QLabel * engage_status_label_ptr_;
-  QPushButton * engage_button_ptr_;
-  QPushButton * velocity_limit_button_ptr_;
-  QPushButton * gate_mode_button_ptr_;
-  QPushButton * path_change_approval_button_ptr_;
-  QSpinBox * pub_velocity_limit_input_;
-  QPushButton * emergency_button_ptr_;
+  // rclcpp::Publisher<tier4_planning_msgs::msg::VelocityLimit>::SharedPtr pub_velocity_limit_;
 
-  bool current_engage_{false};
-  bool current_emergency_{false};
+  QLabel * current_lanelet_label_ptr_;
+  QLabel * history_lanelet_label_ptr_;
+  QLabel * next_lanelet_label_ptr_;
+
+  QTableWidget * current_lanelet_attributes_table_prt_;
+  QTableWidget * history_lanelet_attributes_table_prt_;
+  QTableWidget * next_lanelet_attributes_table_prt_;
+
+
+  QPushButton * teleoperation_button_ptr_;
+  bool teleoperation_state_{false};
+
+  // QLabel * gate_mode_label_ptr_;
+  // QLabel * selector_mode_label_ptr_;
+  // QLabel * autoware_state_label_ptr_;
+  // QLabel * gear_label_ptr_;
+  // QLabel * engage_status_label_ptr_;
+  // QPushButton * engage_button_ptr_;
+  // QPushButton * velocity_limit_button_ptr_;
+  // QPushButton * gate_mode_button_ptr_;
+  // QPushButton * path_change_approval_button_ptr_;
+  // QPushButton * emergency_button_ptr_;
+
+  // bool current_engage_{false};
+  // bool current_emergency_{false};
 };
 
 }  // namespace rviz_plugins
